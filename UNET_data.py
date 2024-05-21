47bf8e6b714a623f3c9d6e79df0e68fc9bdabad7
Maxence <94793600+maxencebellanger@users.noreply.github.com>
2024-05-21 19:06:45 +0200

    reorganisation

---
+++ UNET_data.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created by @sagnik1511 (https://github.com/sagnik1511). 
+All rights reserved.
+
+After setting the images into specified folder directory,
+we are going to take the images into the dataset
+that will be used for training.
+
+"""
+import numpy as np
+import cv2
+import os
+
+HEIGHT = 512
+WIDTH = 512
+
+train_root = 'train_images/'
+test_root = 'test_images/'
+
+images = os.listdir(train_root+"images")
+tests = os.listdir(test_root)
+nb_train_samples = len(images)
+nb_test_samples = len(tests)
+
+X_train = np.zeros((nb_train_samples,HEIGHT,WIDTH,1))
+y_train = np.zeros((nb_train_samples,HEIGHT,WIDTH,1))
+X_test = np.zeros((nb_test_samples,HEIGHT,WIDTH,1))
+
+
+for i in range(nb_train_samples):
+  path = images[i]
+  img = cv2.imread(train_root+"images/"+path)
+  img = cv2.resize(img, (HEIGHT,WIDTH))
+  X_train[i] = img[:,:,0].reshape(512,512,1)
+  img = cv2.imread(train_root+'masks/'+path)
+  img = cv2.resize(img, (HEIGHT,WIDTH))
+  y_train[i] = img[:,:,0].reshape(512,512,1)
+
+
+for i in range(nb_test_samples):
+  path = tests[i]
+  img = cv2.imread(test_root+path)
+  img = cv2.resize(img, (HEIGHT,WIDTH))
+  X_test[i] = img[:,:,0].reshape(512,512,1)
+  
+  
+X_train /= 255.0
+X_test /= 255.0
+y_train /= 255.0
+
+
+np.save('train_image_array.npy',X_train)
+np.save('train_mask_array.npy',y_train)
+np.save('test_image_array.npy',X_test)
---
+++ UNET_directory.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+
+
+""""
+Created by @sagnik1511 (https://github.com/sagnik1511). 
+All rights reserved.
+
+We are taking the data from a specified folder sequence 
+to a usable directory tree format.
+
+
+"""
+import os
+import numpy as np
+import shutil as sh
+from UNET_utils import *
+train_root = 'train_images/'
+test_root = 'test_images/'
+
+train_img_dir = []
+test_img_dir = []
+train_mask_dir = []
+
+
+for i in os.listdir(train_root+'images/'):
+  train_img_dir.append(train_root+'images/'+str(i))
+  train_mask_dir.append(train_root+'masks/'+str(i))
+for i in os.listdir(test_root):
+  test_img_dir.append(test_root+str(i)+'.png')
+
+change_dir( train_img_dir ,'/content/train/images/' )
+change_dir( train_mask_dir ,'/content/train/masks/' )
+change_dir( test_img_dir ,'/content/test/' )
+
