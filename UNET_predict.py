108d673be6e5ce5413e8e07540acbe101ae1d9bc
Maxence <94793600+maxencebellanger@users.noreply.github.com>
2024-05-23 23:34:11 +0200

    update predict (still not working)

--- UNET_predict.py
+++ UNET_predict.py
@@ -6,8 +6,9 @@ import cv2
 def predict_masks():
     model = keras.models.load_model("../model_files/Unet.keras")
     test_images = np.load('../model_files/test_image_array.npy')
+    predicted_masks = model.predict(test_images)
     for i in range(len(test_images)):
-        cv2.imwrite("../training/predicted_masks/"+str(i)+".png", model.predict(test_images[i]))
+        cv2.imwrite("../training/predicted_masks/"+str(i)+".png", predicted_masks[i])
 
 
 predict_masks()
